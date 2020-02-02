47dad6f849be7670a914ac189d7404d9d811b304
ZHENGQING ZHU <zhuzhengqing321@qq.com>
2020-02-02 17:42:22 +0800

    no message

--- src/com/mycomp/requestcontext/RequestContextServlet1.java
+++ src/com/mycomp/requestcontext/RequestContextServlet1.java
@@ -21,7 +21,7 @@ public class RequestContextServlet1 extends HttpServlet {
         // 什么是域对象: 能够存取数据的对象
         // 域对象都有这些方法: setAttribute(), getAttribute(), removeAttribute()
         // 域对象都有作用域
-        // 之前的ServletContext的作用域是整个web应用,从init方法开始到web应用关闭结束
+        // 之前的ServletContext的作用域是整个web应用,从web应用创建开始到web应用关闭结束
         // RequestContext的作用域是一次请求
         // (包含请求转发的中间过程, 但不包括重定向的中间过程, 因为重定向本质上是发送了多个请求)
 
